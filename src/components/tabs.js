d49e5d70d394bc2a000ef19640226f18afbb613a
Alexander Monteiro <alexandermonteiro@Priscilas-MBP.attlocal.net>
2022-07-15 10:02:23 -0700

    tasks1-2-3

--- src/components/tabs.js
+++ src/components/tabs.js
@@ -1,4 +1,19 @@
+import axios from "axios";
+
 const Tabs = (topics) => {
+  const topicsDiv = document.createElement("div");
+
+  topicsDiv.className = "topics";
+
+  topics.forEach((topic) => {
+    const tab = document.createElement("div");
+    tab.className = "tab";
+    tab.textContent = topic;
+    topicsDiv.append(tab);
+  });
+
+  return topicsDiv;
+
   // TASK 3
   // ---------------------
   // Implement this function which takes an array of strings ("topics") as its only argument.
@@ -13,9 +28,20 @@ const Tabs = (topics) => {
   //   <div class="tab">technology</div>
   // </div>
   //
-}
+};
 
 const tabsAppender = (selector) => {
+  axios
+    .get(`http://localhost:5001/api/topics`)
+    .then((res) => {
+      const array = res.data.topics;
+      const tab = document.querySelector(selector);
+      tab.appendChild(Tabs(array));
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+
   // TASK 4
   // ---------------------
   // Implement this function which takes a css selector as its only argument.
@@ -23,6 +49,6 @@ const tabsAppender = (selector) => {
   // Find the array of topics inside the response, and create the tabs using the Tabs component.
   // Append the tabs to the element in the DOM that matches the selector passed to the function.
   //
-}
+};
 
-export { Tabs, tabsAppender }
+export { Tabs, tabsAppender };
